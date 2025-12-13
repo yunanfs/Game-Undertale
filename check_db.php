04a52e025dcf17f86e8d71dbd7744c71975cd580
YUNAN FAILA SOFI <yunanpkl1@gmail.com>
2025-12-14 02:08:03 +0700

    cut-cek

--- check_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'undertale_game');
-$result = $conn->query("SELECT * FROM music");
-while ($row = $result->fetch_assoc()) {
-    echo "Title: " . $row['title'] . "\n";
-    echo "File Path: " . $row['file_path'] . "\n";
-}
-?>
